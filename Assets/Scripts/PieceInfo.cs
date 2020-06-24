64303e5b50e9ab075bfe090c0fa2ea746daf5eb0
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2020-06-23 20:06:35 -0400

    PieceInfo Scriptable Object created

---
+++ Assets/Scripts/PieceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class PieceInfo : ScriptableObject
+{
+    public string name;//the name of the piece
+    public Vector3[] legalMoves;
+    public Vector3[] promotedLegalMoves;
+   //for the moves, the xy coordinates are the actual placements of the move; the z coordinate simply denotes whether the move is finite (0) or infinite (1)
+  
+}
